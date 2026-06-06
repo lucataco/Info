10c38027a9de1675e6ddc53d6bf0ae2bfb480898
Luis Catacora <lucataco@Zeus.cataco>
2026-06-05 20:42:08 -0400

    Initial Commit

---
+++ Info/InfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoApp.swift
+//  Info
+//
+//  Created by Luis Catacora on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct InfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
